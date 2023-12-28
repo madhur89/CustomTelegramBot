dba77d14774c729bc402b4aa39958ac3a34f0e03
Madhur <68078506+madhur89@users.noreply.github.com>
2023-12-28 10:39:43 +0530

    Update main.py
    Fixed negative issue

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ previous_member_counts = {group_name: 0 for group_name in GROUP_CHAT_IDS}
 def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text("Bot is active. You will receive notifications for new joins and leaves.")
     # Schedule the job to send member count every 20 seconds to the admin group
-    context.job_queue.run_repeating(send_member_count, interval=20, first=0, context=(None, ADMIN_GROUP_CHAT_ID))
+    context.job_queue.run_repeating(send_member_count, interval=200, first=0, context=(None, ADMIN_GROUP_CHAT_ID))
+    
 
 def new_member(update: Update, context: CallbackContext) -> None:
     user = update.message.new_chat_members[0]
@@ -44,14 +45,15 @@ def send_member_count(context: CallbackContext) -> None:
     # Calculate and send the member count changes to the admin group
     for group_name, group_chat_id in GROUP_CHAT_IDS.items():
         current_member_count = context.bot.get_chat_member_count(chat_id=group_chat_id)
-        joined_count = current_member_count - previous_member_counts[group_name]
-        left_count = previous_member_counts[group_name] - current_member_count
+        joined_count = max(0, current_member_count - previous_member_counts[group_name])
+        left_count = max(0, previous_member_counts[group_name] - current_member_count)
 
         message = f"{group_name} - Members Joined in 24hrs: {joined_count}, Members Left in 24hrs: {left_count}"
         context.bot.send_message(chat_id=admin_group_chat_id, text=message)
 
         # Update the previous member count for the next interval
         previous_member_counts[group_name] = current_member_count
+
 
 def chat_member(update: Update, context: CallbackContext) -> None:
     pass  # You can customize this function if needed
